c20e4f367aad9d2d2a04a9ce487a7ec78633352d
Adam Walker <adamwalker10@gmail.com>
2016-12-29 21:49:12 +1100

    bcd converter, cabalize

---
+++ src/CLaSH/BCD.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module CLaSH.BCD where
+
+import CLaSH.Prelude
+
+type BCDDigit = Unsigned 4
+
+add3 :: BCDDigit -> BCDDigit
+add3 digit
+    | digit > 4 = digit + 3
+    | otherwise = digit
+
+convertStep 
+    :: forall n. (KnownNat (n * 4), KnownNat n) 
+    => Bit 
+    -> Vec n BCDDigit 
+    -> Vec n BCDDigit
+convertStep bit digits = unpack $ pack shifted
+    where
+    --dabble
+    dabbled   =  map add3 digits
+    --Flatten the BCD to a vector of bits
+    flattened :: Vec (n * 4) Bit
+    flattened = unpack $ pack dabbled
+    --shift in the input bit
+    shifted   = flattened <<+ bit
+
+singleCycleConvert 
+    :: (KnownNat n, KnownNat m, KnownNat (m * 4))
+    => BitVector n
+    -> Vec m BCDDigit
+    -> Vec m BCDDigit
+singleCycleConvert n d = foldl (flip convertStep) d (unpack n)
+
+toDec :: (KnownNat n, KnownNat m, KnownNat (m * 4)) => BitVector n -> Vec m BCDDigit
+toDec = flip singleCycleConvert (repeat 0)
