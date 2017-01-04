c90fe64f9acf78b2b54c87564fa8ec750816c609
Adam Walker <adamwalker10@gmail.com>
2017-01-04 20:00:25 +1100

    fifo

---
+++ src/CLaSH/FIFO.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module CLaSH.FIFO where
+
+import CLaSH.Prelude
+
+blockRamFIFO 
+    :: forall size a. (KnownNat size, Default a)
+    => SNat size   -- ^ FIFO size
+    -> Signal Bool -- ^ Read request
+    -> Signal a    -- ^ Write data
+    -> Signal Bool -- ^ Write request
+    -> (
+        Signal a,    -- ^ Read data
+        Signal Bool, -- ^ Empty
+        Signal Bool  -- ^ Full
+    )
+blockRamFIFO size rReq wData wReq = (ramOut, empty, full)
+    where
+    --The backing ram
+    ramOut = blockRam (repeat def :: Vec size a) wAddr rAddr wEn wData
+    --The status signals
+    empty  = wAddr .==. rAddr
+    full   = rAddr .==. (wrappingInc <$> wAddr)
+    --Only write if there is space
+    wEn    = wReq .&&. not1 full
+    --The read and write pointers
+    wAddr, rAddr :: Signal (Index size)
+    wAddr = register 0 $ mux wEn                    (wrappingInc <$> wAddr) wAddr
+    rAddr = register 0 $ mux (rReq .&&. not1 empty) (wrappingInc <$> rAddr) rAddr
+
+    wrappingInc :: Index size -> Index size
+    wrappingInc val
+        | val == maxBound = 0
+        | otherwise       = val + 1
