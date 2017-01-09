2498bb303c98fab9df0ab8a0ad88baffd6dfadf9
Adam Walker <adamwalker10@gmail.com>
2017-01-09 15:58:56 +1100

    parallel crc

--- src/CLaSH/CRC.hs
+++ src/CLaSH/CRC.hs
@@ -3,7 +3,9 @@
 {-| <https://en.wikipedia.org/wiki/Cyclic_redundancy_check Cyclic redundancy check> -}
 module CLaSH.CRC (
     crcStep,
-    serialCRC
+    crcSteps,
+    serialCRC,
+    parallelCRC
     ) where
 
 import CLaSH.Prelude
@@ -20,6 +22,16 @@ serialCRC init polynomial input = pack <$> mealy step' (unpack init) input
     where
     step' st inp = (x, x) where x = crcStep polynomial st inp
 
+parallelCRC 
+    :: forall n m. (KnownNat n, KnownNat (n + 1), KnownNat m)
+    => BitVector (n + 1)          -- ^ Initial value of shift register
+    -> BitVector n                -- ^ The polynomial
+    -> Signal (BitVector m)       -- ^ Input bit
+    -> Signal (BitVector (n + 1)) -- ^ CRC
+parallelCRC init polynomial input = pack <$> mealy step' (unpack init) input
+    where
+    step' st inp = (x, x) where x = crcSteps polynomial st inp
+
 crcStep 
     :: (KnownNat n)
     => BitVector n     -- ^ Polynomial
@@ -30,4 +42,12 @@ crcStep polynomial (head :> rest) inp = zipWith selectIn (unpack polynomial) res
     where
     rightmostBit = inp `xor` head
     selectIn sel bit =  bool bit (bit `xor` rightmostBit) sel
+
+crcSteps 
+    :: forall n m. (KnownNat n, KnownNat m)
+    =>  BitVector n     -- ^ Polynomial
+    -> Vec (n + 1) Bit -- ^ Shift register state
+    -> BitVector m     -- ^ Input bits
+    -> Vec (n + 1) Bit -- ^ Next shift register state
+crcSteps polynomial state input = foldl (crcStep polynomial) state (unpack input :: Vec m Bit)
 
