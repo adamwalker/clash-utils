0673de37d4f7c6d6667d1ddd8d36a178a0c1e6a9
Adam Walker <adamwalker10@gmail.com>
2018-02-09 23:53:36 +1100

    Cuckoo hashtable docs

--- src/Clash/Cuckoo.hs
+++ src/Clash/Cuckoo.hs
@@ -1,4 +1,9 @@
 {-# LANGUAGE RecordWildCards #-}
+
+{-| Implements the lookup side of a <https://en.wikipedia.org/wiki/Cuckoo_hashing Cuckoo hashtable>.
+
+    Cuckoo hashtables are well suited to communicating key value pairs with hardware where the space of keys is large.
+ -}
 module Clash.Cuckoo (
     TableEntry(..),
     cuckoo
@@ -11,12 +16,13 @@ data TableEntry k v = TableEntry {
     value :: v
 }
 
+{-| The lookup side of a Cuckoo hashtable. Uses split tables. -}
 cuckoo 
     :: forall dom gated sync m n k v. (HasClockReset dom gated sync, KnownNat n, Eq k)
-    => Vec (m + 1) (k -> Unsigned n)
-    -> Vec (m + 1) (Signal dom (Maybe (Unsigned n, Maybe (TableEntry k v))))
-    -> Signal dom k
-    -> Signal dom (Maybe v)
+    => Vec (m + 1) (k -> Unsigned n)                                         -- ^ Vector of hash functions, one for each table. 
+    -> Vec (m + 1) (Signal dom (Maybe (Unsigned n, Maybe (TableEntry k v)))) -- ^ Table updates from software. A vector of (row number, row value) pair updates, one for each table. 
+    -> Signal dom k                                                          -- ^ The key to lookup.
+    -> Signal dom (Maybe v)                                                  -- ^ The result of the lookup. Will be ready one cycle after the key is input. 
 cuckoo hashFunctions tableUpdates lookupKey = fold (liftA2 (<|>)) candidates
     where
 
