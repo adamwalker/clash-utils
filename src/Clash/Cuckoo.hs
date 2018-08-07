f93363da39ff7e710b0230389b3741a3597ef0d4
Adam Walker <adamwalker10@gmail.com>
2018-08-07 21:22:59 +1000

    cuckoo hashtable with insertions

--- src/Clash/Cuckoo.hs
+++ src/Clash/Cuckoo.hs
@@ -7,11 +7,14 @@
 module Clash.Cuckoo (
     TableEntry(..),
     cuckoo',
-    cuckoo
+    cuckoo,
+    cuckooWithInsert
     ) where
 
 import GHC.Generics
 import Clash.Prelude
+import Clash.Prelude.Moore
+import Data.Maybe
 
 data TableEntry k v = TableEntry {
     key   :: k,
@@ -57,4 +60,117 @@ cuckoo hashFunctions tableUpdates lookupKey = cuckoo' tableUpdates hashes lookup
     --compute all the hashes
     hashes :: Vec (m + 1) (Signal dom (Unsigned n))
     hashes = sequenceA $ hashFunctions <$> lookupKey
+
+cuckooWithInsert
+    :: forall dom gated sync m n k v. (HiddenClockReset dom gated sync, KnownNat m, KnownNat n, Eq k)
+    => (k -> Vec (m + 1) (Unsigned n))                                       
+    -> Signal dom k                                                          
+    -> Signal dom v
+    -> Signal dom Bool
+    -> (
+        Signal dom (Maybe (Index (m + 1), Unsigned n, v)), -- Table index, table row, value
+        Signal dom Bool, 
+        Signal dom Bool
+        )
+cuckooWithInsert hashFunctions key' value' insert = (lookupResult, isJust <$> writebackStage, insertingDone)
+    where
+
+    --------------------------------------------------------------------------------
+    --Insertion logic
+    --------------------------------------------------------------------------------
+
+    --Insert/modify/delete begin by looking up the entry
+    lookupStage :: Signal dom (TableEntry k v)
+    lookupStage =  mux
+        ((isJust <$> writebackStage) .&&. not <$> insertingDone)
+        evictedEntry
+        (TableEntry <$> key' <*> value')
+
+    writebackStage :: Signal dom (Maybe (TableEntry k v))
+    writebackStage =  medvedevB step Nothing (insert, insertingDone, lookupStage)
+        where
+        step Nothing  (True,   _,     lookupStage) = Just lookupStage
+        step Nothing  (False,  _,     _)           = Nothing
+        step (Just _) (_,      True,  _)           = Nothing
+        step (Just _) (_,      False, lookupStage) = Just lookupStage
+
+    insertingDone :: Signal dom Bool
+    insertingDone =  (isJust <$> freeSlot) .||. (firstInsertCycle .&&. isJust <$> lookupResult)
+
+    --Find a free slot to replace, if any
+    freeSlot :: Signal dom (Maybe (Index (m + 1)))
+    freeSlot =  findIndex isNothing <$> sequenceA fromMem
+    --freeSlot =  fmap (join . find isJust) $ rotateLeft <$> (imap func <$> sequenceA fromMem) <*> toEvict
+    --    where
+    --    func idx Nothing = Just idx
+    --    func _   _       = Nothing
+
+    --Freerunning counter for randomly choosing table to evict from
+    toEvict :: Signal dom (Index (m + 1))
+    toEvict =  register 0 $ func <$> toEvict
+        where
+        func x
+            | x == maxBound = 0
+            | otherwise     = x + 1
+
+    --Select the table entry to evict
+    --This will become the value to insert in the next cycle
+    --TODO: figure out how to get rid of fromJust 
+    evictedEntry :: Signal dom (TableEntry k v)
+    evictedEntry =  fmap fromJust $ (!!) <$> sequenceA fromMem <*> toEvict
+
+    --Track if this is the first insert writeback cycle. If it is we must consider the case where the key was already in the hashtable
+    firstInsertCycle = register False $ insert .&&. ((isNothing <$> writebackStage) .||. insertingDone)
+
+    --Calculate the index to update. Prefer free slot, otherwise evict.
+    indexToUpdate :: Signal dom (Index (m + 1))
+    indexToUpdate =  selectIndex <$> firstInsertCycle <*> lookupResult <*> freeSlot <*> toEvict
+        where
+        selectIndex True (Just (idx, _, _)) _               _       = idx      --We are modifying an existing entry
+        selectIndex _    _                  (Just freeSlot) _       = freeSlot --We found a free slot
+        selectIndex _    _                  _               toEvict = toEvict  --No free slots, so we are evicting
+
+    --Save the hashes of the evicted entry
+    hashesD :: Vec (m + 1) (Signal dom (Unsigned n))
+    hashesD =  map (register (errorX "initial registered hashes")) hashes
+
+    --Update the chosen index if we are inserting with either the data to insert or the previously evicted table entry
+    tableUpdates :: Vec (m + 1) (Signal dom (Maybe (Unsigned n, Maybe (TableEntry k v))))
+    tableUpdates =  map (\idx -> genTableUpdate idx <$> indexToUpdate <*> writebackStage <*> sequenceA hashesD) $ iterateI (+1) 0
+        where
+        genTableUpdate
+            :: Index (m + 1) 
+            -> Index (m + 1) 
+            -> Maybe (TableEntry k v)
+            -> Vec (m + 1) (Unsigned n)
+            -> Maybe (Unsigned n, Maybe (TableEntry k v))
+        genTableUpdate currIdx idxToUpdate toInsert tableRow
+            | Just toInsert <- toInsert, currIdx == idxToUpdate = Just (tableRow !! currIdx, Just toInsert)
+            | otherwise                                         = Nothing
+
+    --------------------------------------------------------------------------------
+    --This stuff is common to lookups, deletes and inserts
+    --------------------------------------------------------------------------------
+
+    --Calculate the lookup hashes
+    hashes :: Vec (m + 1) (Signal dom (Unsigned n))
+    hashes =  sequenceA $ hashFunctions . key <$> lookupStage
+
+    --Do the lookups
+    fromMem :: Vec (m + 1) (Signal dom (Maybe (TableEntry k v)))
+    fromMem =  zipWith (blockRamPow2 (repeat Nothing)) hashes tableUpdates
+
+    --Check if each item returned from memory matches
+    candidates :: Vec (m + 1) (Signal dom (Maybe (Index (m + 1), Unsigned n, v)))
+    candidates =  izipWith (\idx -> liftA3 (checkCandidate idx) (register (errorX "key") key')) (map (register (errorX "initial hashes")) hashes) fromMem 
+        where
+        --Get rid of signals
+        checkCandidate :: Index (m + 1) -> k -> Unsigned n -> Maybe (TableEntry k v) -> Maybe (Index (m + 1), Unsigned n, v)
+        checkCandidate _   lookupKey _    Nothing = Nothing
+        checkCandidate idx lookupKey hash (Just TableEntry{..}) 
+            | lookupKey == key = Just (idx, hash, value)
+            | otherwise        = Nothing
+
+    lookupResult :: Signal dom (Maybe (Index (m + 1), Unsigned n, v))
+    lookupResult =  fold (liftA2 (<|>)) candidates
 
--- tests/CuckooSpec.hs
+++ tests/CuckooSpec.hs
@@ -3,14 +3,17 @@ module CuckooSpec where
 import qualified Clash.Prelude as Clash
 import Clash.Prelude (Signal, Vec(..), BitVector, Index, Signed, Unsigned, SFixed, Bit, SNat(..),
                       simulate, simulate_lazy, listToVecTH, KnownNat, pack, unpack, (++#), mealy, mux, bundle, unbundle, 
-                      HiddenClockReset)
+                      HiddenClockReset, (.==.), sampleN_lazy, register, regEn, (.<.), (.||.), (.&&.), iterateI)
 
 import Test.Hspec
-import Test.QuickCheck
+import Test.QuickCheck hiding ((.||.), (.&&.))
 
 import Data.Hashable
 import Data.List
 import Clash.Cuckoo
+import qualified Data.Map as Map
+import Data.Map (Map)
+import Data.Tuple.All
 
 spec = describe "Cuckoo hash table" $ do
 
@@ -24,6 +27,8 @@ spec = describe "Cuckoo hash table" $ do
 
     specify "Deletes elements" $ property $ forAll (choose (0, 2)) $ \idx k v -> 
         simulate_lazy system (testVecDelete idx k v) !! 3 == Nothing
+
+    specify "cuckooWithInsert works" $ property testInserts
 
 system 
     :: HiddenClockReset dom gated sync 
@@ -84,4 +89,44 @@ testVecDelete idx k v = [write, delete, lookup, null]
     delete = (Clash.replace idx (Just (hashFunc idx k, Nothing))               (Clash.repeat Nothing), "")
     lookup = (Clash.repeat Nothing, k)
     null   = (Clash.repeat Nothing, "")
+
+insertTestHarness
+    :: forall dom gated sync. HiddenClockReset dom gated sync
+    => [(Int, String)]
+    -> Signal dom (Bool, Bool)
+insertTestHarness vals = bundle (register True insertingPhase .||. success, done)
+    where
+
+    inserted :: Signal dom Int
+    inserted = regEn 0 (inserting .&&. insertingDone) (inserted + 1)
+    
+    (lookupResult, inserting, insertingDone) = cuckooWithInsert hashFuncs insertKey insertValue insert
+
+    hashFuncs :: Int -> Vec 3 (Unsigned 10)
+    hashFuncs x = Clash.map (\idx -> fromIntegral $ (`mod` 1024) $ hashWithSalt idx x) (iterateI (+1) 0)
+
+    (insertKey', insertValue, insert') = unbundle $ mealy step vals inserting
+        where
+        step :: [(Int, String)] -> Bool -> ([(Int, String)], (Int, String, Bool))
+        step []             _     = ([],   (0, "", False))
+        step ((k,v) : rest) False = (rest, (k,   v, True))
+        step st             True  = (st,   (0, "", False))
+
+    insertingPhase = (inserted .<. 2800) .||. inserting
+    insert         = insert' .&&. insertingPhase
+    insertKey      = mux insertingPhase insertKey' toLookup
+
+    (done, lookupsRemaining, toLookup) = unbundle $ register (False, Prelude.take 2800 vals, 0) $ func <$> insertingPhase <*> lookupsRemaining
+        where
+        func True  lookupsRemaining = (False, lookupsRemaining, 0)
+        func False []               = (True,  [], 0)
+        func False (x:xs)           = (False, xs, fst x)
+    expect                   = register Nothing $ flip Map.lookup (Map.fromList (Prelude.take 2800 vals)) <$> insertKey
+    success                  = expect .==. fmap (fmap sel3) lookupResult
+
+testInserts (InfiniteList insertSeq _) = finished && success
+    where
+    res      = sampleN_lazy 50000 $ insertTestHarness insertSeq
+    finished = elem True $ Prelude.map snd res
+    success  = all id $ Prelude.map fst res
 
