82d6e474ff295c8c451d2e10e638d861bce3a3de
Adam Walker <adamwalker10@gmail.com>
2019-04-03 21:58:37 +1100

    AES encryption

---
+++ tests/AESSpec.hs
@@ -0,0 +1,31 @@
+module AESSpec where
+
+import qualified Clash.Prelude as Clash
+import Clash.Prelude (Signal, Vec(..), BitVector, Index, Signed, Unsigned, SFixed, Bit, SNat(..),
+                      simulate, simulate_lazy, listToVecTH, KnownNat, pack, unpack, (++#), mealy, mux, bundle, unbundle, 
+                      HiddenClockReset, toList, sampleN, fromList)
+import Test.Hspec
+import Test.QuickCheck
+
+import Crypto.Cipher.AES
+import Clash.Crypto.AES
+
+import qualified Data.ByteString as BS
+import Data.ByteString (ByteString)
+import qualified Data.List as Prelude
+
+--BCD conversion testing
+spec :: SpecWith ()
+spec = do
+    describe "AES" $ 
+        it "encrypts" $ property prop_AES
+
+prop_AES :: BitVector 128 -> BitVector 128 -> Bool
+prop_AES key block = fmap (fmap toBS) res == Just (True, expect)
+    where
+    toBS x  = BS.pack $ Prelude.map fromIntegral $ toList $ (unpack x :: Vec 16 (BitVector 8))
+    keyBS   = toBS key
+    blockBS = toBS block
+    expect  = encryptECB (initAES keyBS) blockBS
+    res     = Prelude.find fst $ sampleN 20 (aesEncrypt starts (pure key) (pure block)) 
+    starts  = fromList $ False : True : Prelude.repeat False
