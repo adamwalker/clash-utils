7634123550848a5beef1c2a1f5145549ecbbce19
Adam Walker <adamwalker10@gmail.com>
2019-08-02 23:29:14 +1000

    Carry save adder

---
+++ src/Clash/Arithmetic/CarrySave.hs
@@ -0,0 +1,41 @@
+module Clash.Arithmetic.CarrySave (
+        fullAdder,
+        compressor,
+        compressorN
+    ) where
+
+import Clash.Prelude
+import Data.Tuple
+
+fullAdder :: Bool -> Bool -> Bool -> (Bool, Bool)
+fullAdder cIn x y = unpack $ (resize (pack cIn) :: BitVector 2) + resize (pack x) + resize (pack y)
+
+compressor 
+    :: forall carrys
+    .  Vec carrys Bool
+    -> Vec (carrys + 3) Bool
+    -> (Vec carrys Bool, Vec 2 Bool)
+compressor cIns toSum = (carrys, msb :> lsb :> Nil)
+    where
+    s1 :> s2 :> s3 :> sRest = toSum
+
+    (lsb, carrys :< msb) = mapAccumL func s1 $ (s2, s3) :> zip cIns sRest
+        where
+        func :: Bool -> (Bool, Bool) -> (Bool, Bool)
+        func x (c, y) = swap $ fullAdder c x y
+
+compressorN
+    :: forall carrys n
+    .  (KnownNat n)
+    => Vec carrys Bool
+    -> Vec (carrys + 3) (BitVector n)
+    -> (Vec carrys Bool, Vec 2 (BitVector n))
+compressorN cIns toSum = (cOuts, pack (reverse resMsb) :> pack (reverse resLsb) :> Nil)
+    where
+    toSumVec :: Vec n (Vec (carrys + 3) Bool)
+    toSumVec =  transpose $ map (reverse . unpack) toSum
+
+    (cOuts, sumVecs) = mapAccumL compressor cIns toSumVec
+
+    (resMsb :> resLsb :> Nil) = transpose sumVecs
+
---
+++ tests/CarrySaveSpec.hs
@@ -0,0 +1,24 @@
+module CarrySaveSpec where
+
+import qualified Clash.Prelude as Clash
+import Clash.Prelude (Signal, Vec(..), BitVector, Index, Signed, Unsigned, SFixed, Bit, SNat(..),
+                      simulate, simulate_lazy, listToVecTH, KnownNat, pack, unpack, (++#), mealy, mux, bundle, unbundle, 
+                      HiddenClockReset, extend, shiftL)
+import Test.Hspec
+import Test.QuickCheck
+
+import Clash.Arithmetic.CarrySave
+
+--Misc
+spec = describe "Carry save" $ do
+    specify "is correct" $ property prop_carrySave
+
+prop_carrySave :: Vec 4 (BitVector 8) -> Bool
+prop_carrySave xs = expect == result
+    where
+    (carry :> Nil, (r1 :> r2 :> Nil)) = compressorN (Clash.repeat False) xs 
+    expect :: BitVector 10 
+    expect =  sum $ Clash.map extend xs
+    result :: BitVector 10
+    result =  (extend r1 `shiftL` 1) + extend r2 + (extend (pack carry) `shiftL` 8)
+
