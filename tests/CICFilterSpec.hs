60f56b0a88e413d7309cc97e8b16fa23aba0c499
Adam Walker <adamwalker10@gmail.com>
2019-08-25 22:12:26 +1000

    2nd order CIC test

--- tests/CICFilterSpec.hs
+++ tests/CICFilterSpec.hs
@@ -10,15 +10,16 @@ import Test.QuickCheck hiding (sample)
 import Clash.DSP.CICFilter
 
 spec = describe "CIC filters" $ do
-    specify "1st order is equivalent to moving sum" $ property $ prop_cic_sums
+    specify "1st order is equivalent to moving sum" $ property $ prop_cicSums
+    specify "2nd order" $ property $ prop_cicSecondOrder
 
 groups :: Int -> Int -> [a] -> [[a]]
 groups t d x = take t x : groups t d (drop d x)
 
-prop_cic_sums :: InfiniteList (BitVector 8) -> Bool
-prop_cic_sums (InfiniteList x _) = expect == result
+prop_cicSums :: InfiniteList (BitVector 8) -> Bool
+prop_cicSums (InfiniteList x _) = expect == result
     where
-    extended :: [BitVector 10]
+    extended :: [BitVector 11]
     extended =  map extend x
     expect   
         = take 64 
@@ -32,3 +33,23 @@ prop_cic_sums (InfiniteList x _) = expect == result
         $ sample @System
         $ bundle 
         $ cicDecimate (SNat @ 4) (SNat @ 2) (SNat @ 1) (pure True) (fromList (0 : extended))
+
+prop_cicSecondOrder :: InfiniteList (BitVector 8) -> Bool
+prop_cicSecondOrder (InfiniteList x _) = expect == result
+    where
+    extended :: [BitVector 14]
+    extended =  map extend x
+    expect   
+        = take 64 
+        $ map sum 
+        $ groups 8 4
+        $ map sum
+        $ groups 8 1 extended
+    result   
+        = take 64 
+        $ drop 7
+        $ map snd 
+        $ filter fst 
+        $ sample @System
+        $ bundle 
+        $ cicDecimate (SNat @ 4) (SNat @ 2) (SNat @ 2) (pure True) (fromList (0 : extended))
