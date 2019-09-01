2b3d16c11eb0833140f8bd73c3a918b5a955fea6
Adam Walker <adamwalker10@gmail.com>
2019-09-02 01:33:52 +1000

    FIFOs output number of stored items

--- src/Clash/Container/FIFO.hs
+++ src/Clash/Container/FIFO.hs
@@ -16,48 +16,47 @@ import Data.Maybe
 
 {-| A FIFO backed by block ram. The input does not fall through, i.e. if the FIFO is empty and a value is written to it, that value is not available on the output in the same cycle. A current limitation of this FIFO is that its capacity is one less than the capacity of the underlying block ram. -}
 blockRamFIFO 
-    :: forall dom size a. (HiddenClockResetEnable dom, KnownNat size, Undefined a)
-    => SNat size   -- ^ FIFO size
+    :: forall dom sizeBits a. (HiddenClockResetEnable dom, Undefined a)
+    => SNat sizeBits   -- ^ FIFO size
     -> Signal dom Bool -- ^ Read request
     -> Signal dom a    -- ^ Write data
     -> Signal dom Bool -- ^ Write request
     -> (
-        Signal dom a,    -- Read data
-        Signal dom Bool, -- Empty
-        Signal dom Bool  -- Full
+        Signal dom a,                  -- Read data
+        Signal dom Bool,               -- Empty
+        Signal dom Bool,               -- Full
+        Signal dom (Unsigned sizeBits) -- Num elements
     )
-blockRamFIFO size rReq wData wReq = (ramOut, empty, full)
+blockRamFIFO SNat rReq wData wReq = (ramOut, empty, full, nElements)
     where
     --The backing ram
-    ramOut = blockRam (repeat (errorX "Initial FIFO ram contents") :: Vec size a) rAddr' (mux wEn (Just <$> bundle (wAddr, wData)) (pure Nothing))
+    ramOut = blockRamPow2 (repeat (errorX "Initial FIFO ram contents")) rAddr' (mux wEn (Just <$> bundle (wAddr, wData)) (pure Nothing))
     --The status signals
     empty  = (register 0 wAddr) .==. rAddr
-    full   = rAddr .==. (wrappingInc <$> wAddr)
+    full   = rAddr .==. (wAddr + 1)
     --Only write if there is space
     wEn    = wReq .&&. fmap not full
     rEn    = rReq .&&. fmap not empty
     --The read and write pointers
-    wAddr, rAddr :: Signal dom (Index size)
-    wAddr  = register 0 $ mux wEn (wrappingInc <$> wAddr) wAddr
-    rAddr' = mux rEn (wrappingInc <$> rAddr) rAddr
+    wAddr, rAddr :: Signal dom (Unsigned sizeBits)
+    wAddr  = register 0 $ mux wEn (wAddr + 1) wAddr
+    rAddr' = mux rEn (rAddr + 1) rAddr
     rAddr  = register 0 rAddr'
-
-    wrappingInc :: Index size -> Index size
-    wrappingInc val
-        | val == maxBound = 0
-        | otherwise       = val + 1
+    --Number of elements
+    nElements = wAddr - rAddr
 
 {-| Same as `blockRamFIFO` but uses Maybe to tag values to read/write -}
 blockRamFIFOMaybe
-    :: forall dom size a. (HiddenClockResetEnable dom, KnownNat size, Undefined a)
-    => SNat size          -- ^ FIFO size
+    :: forall dom sizeBits a. (HiddenClockResetEnable dom, Undefined a)
+    => SNat sizeBits          -- ^ FIFO size
     -> Signal dom Bool        -- ^ Read request
     -> Signal dom (Maybe a)   -- ^ Write data
     -> (
-        Signal dom (Maybe a), -- Read data
-        Signal dom Bool       -- Full
+        Signal dom (Maybe a),          -- Read data
+        Signal dom Bool,               -- Full
+        Signal dom (Unsigned sizeBits) -- Num elements
     )
-blockRamFIFOMaybe size rReq write = (mux empty (pure Nothing) (Just <$> ramOut), full)
+blockRamFIFOMaybe SNat rReq write = (mux empty (pure Nothing) (Just <$> ramOut), full, nElements)
     where
     --Write command
     writeCommand = func <$> wEn <*> write <*> wAddr
@@ -66,21 +65,18 @@ blockRamFIFOMaybe size rReq write = (mux empty (pure Nothing) (Just <$> ramOut),
         func _     Nothing    _     = Nothing
         func _     (Just dat) wAddr = Just (wAddr, dat)
     --The backing ram
-    ramOut = blockRam (repeat (errorX "Initial FIFO ram contents") :: Vec size a) rAddr' writeCommand
+    ramOut = blockRamPow2 (repeat (errorX "Initial FIFO ram contents")) rAddr' writeCommand
     --The status signals
     empty  = (register 0 wAddr) .==. rAddr
-    full   = rAddr .==. (wrappingInc <$> wAddr)
+    full   = rAddr .==. (wAddr + 1)
     --Only write if there is space
     wEn    = fmap not full
     rEn    = rReq .&&. fmap not empty
     --The read and write pointers
-    wAddr, rAddr :: Signal dom (Index size)
-    wAddr  = register 0 $ mux (isJust <$> writeCommand) (wrappingInc <$> wAddr) wAddr
-    rAddr' = mux rEn (wrappingInc <$> rAddr) rAddr
+    wAddr, rAddr :: Signal dom (Unsigned sizeBits)
+    wAddr  = register 0 $ mux (isJust <$> writeCommand) (wAddr + 1) wAddr
+    rAddr' = mux rEn (rAddr + 1) rAddr
     rAddr  = register 0 rAddr'
-
-    wrappingInc :: Index size -> Index size
-    wrappingInc val
-        | val == maxBound = 0
-        | otherwise       = val + 1
+    --Number of elements
+    nElements = wAddr - rAddr
 
--- tests/FIFOSpec.hs
+++ tests/FIFOSpec.hs
@@ -19,8 +19,8 @@ spec = describe "FIFO" $ do
     specify "equivalent to software model" $ property prop_FIFOs
     specify "maybe version equivalent to software model" $ property prop_FIFOMaybe
 
-fifoStep :: Int -> (Int, Seq a) -> (Bool, a, Bool) -> ((Int, Seq a), (a, Bool, Bool))
-fifoStep size (lastSize, storage) (readEn, writeValue, writeEn) = ((nextSize, storage''), (extract storage, empty, full)) 
+fifoStep :: Int -> (Int, Seq a) -> (Bool, a, Bool) -> ((Int, Seq a), (a, Bool, Bool, Int))
+fifoStep size (lastSize, storage) (readEn, writeValue, writeEn) = ((nextSize, storage''), (extract storage, empty, full, nItems)) 
     where
     full             = Seq.length storage == size - 1
     nextSize         = Seq.length storage'
@@ -35,29 +35,35 @@ fifoStep size (lastSize, storage) (readEn, writeValue, writeEn) = ((nextSize, st
     extract :: Seq a -> a
     extract s = case Seq.viewl s of
         (x Seq.:< rest) -> x
+    nItems = Seq.length storage
 
-compareOutputs :: Eq a => (a, Bool, Bool) -> (a, Bool, Bool) -> Bool
-compareOutputs (val1, empty1, full1) (val2, empty2, full2) 
+compareOutputs :: Eq a => (a, Bool, Bool, Int) -> (a, Bool, Bool, Int) -> Bool
+compareOutputs (val1, empty1, full1, size1) (val2, empty2, full2, size2) 
     =  empty1 == empty2 
     && full1  == full2 
     && (empty1 || val1 == val2)
+    && size1 == size2
 
 prop_FIFOs :: [(Bool, BitVector 32, Bool)] -> Bool
 prop_FIFOs signals = and $ zipWith compareOutputs expect result
     where
-    expect = take (length signals) $ simulate_lazy @System (mealy (fifoStep 5) (0, Seq.empty)) signals
+    expect = take (length signals) $ simulate_lazy @System (mealy (fifoStep 8) (0, Seq.empty)) signals
     result = take (length signals) $ simulate_lazy @System hackedFIFO signals
-    hackedFIFO :: HiddenClockResetEnable dom => Signal dom (Bool, BitVector 32, Bool) -> Signal dom (BitVector 32, Bool, Bool)
-    hackedFIFO = bundle . (\(x, y, z) ->  (blockRamFIFO (SNat @ 5)) x y z) . unbundle 
+    hackedFIFO :: HiddenClockResetEnable dom => Signal dom (Bool, BitVector 32, Bool) -> Signal dom (BitVector 32, Bool, Bool, Int)
+    hackedFIFO = bundle . func . unbundle 
+        where
+        func (x, y, z) = 
+            let (dat, empty, full, num) = blockRamFIFO (SNat @ 3) x y z
+            in  (dat, empty, full, fromIntegral <$> num)
 
 prop_FIFOMaybe :: [(Bool, BitVector 32, Bool)] -> Bool
 prop_FIFOMaybe signals = Prelude.and $ Prelude.zipWith compareOutputs expect result
     where
-    expect = take (length signals) $ simulate_lazy @System (mealy (fifoStep 5) (0, Seq.empty)) signals
+    expect = take (length signals) $ simulate_lazy @System (mealy (fifoStep 8) (0, Seq.empty)) signals
     result = take (length signals) $ simulate_lazy @System hackedFIFO signals
-    hackedFIFO :: HiddenClockResetEnable dom => Signal dom (Bool, BitVector 32, Bool) -> Signal dom (BitVector 32, Bool, Bool)
-    hackedFIFO inputs = bundle $ (fromJust <$> readDataM, (not . isJust) <$> readDataM, full)
+    hackedFIFO :: HiddenClockResetEnable dom => Signal dom (Bool, BitVector 32, Bool) -> Signal dom (BitVector 32, Bool, Bool, Int)
+    hackedFIFO inputs = bundle $ (fromJust <$> readDataM, (not . isJust) <$> readDataM, full, fromIntegral <$> nItems)
         where
         (readReq, writeData, writeReq) = unbundle inputs
-        (readDataM, full)              = blockRamFIFOMaybe (SNat @ 5) readReq $ mux writeReq (Just <$> writeData) (pure Nothing)
+        (readDataM, full, nItems)      = blockRamFIFOMaybe (SNat @ 3) readReq $ mux writeReq (Just <$> writeData) (pure Nothing)
 
