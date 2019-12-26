7ee7e5515bed2ef1f701bcddc2da076124bb49d2
Adam Walker <adamwalker10@gmail.com>
2019-12-26 16:41:23 +1100

    derive classes

--- src/Clash/Network/Types.hs
+++ src/Clash/Network/Types.hs
@@ -8,7 +8,7 @@ data EthernetHeader = EthernetHeader {
     destMac   :: Vec 6 (BitVector 8),
     sourceMac :: Vec 6 (BitVector 8),
     etherType :: BitVector 16
-} deriving (Generic, BitPack, Show, ShowX)
+} deriving (Generic, BitPack, Show, ShowX, Eq, Ord)
 
 -- | [IPv4 header](https://en.wikipedia.org/wiki/IPv4)
 data IPv4Header = IPv4Header {
@@ -25,7 +25,7 @@ data IPv4Header = IPv4Header {
     headerChecksum :: BitVector 16,
     sourceIP       :: Vec 4 (BitVector 8),
     destIP         :: Vec 4 (BitVector 8)
-} deriving (Generic, BitPack, Show, ShowX)
+} deriving (Generic, BitPack, Show, ShowX, Eq, Ord)
 
 -- | [UDP header](https://en.wikipedia.org/wiki/User_Datagram_Protocol)
 data UDPHeader = UDPHeader {
@@ -33,5 +33,5 @@ data UDPHeader = UDPHeader {
     destPort    :: BitVector 16,
     udpLen      :: BitVector 16,
     udpChecksum :: BitVector 16
-} deriving (Generic, BitPack, Show, ShowX)
+} deriving (Generic, BitPack, Show, ShowX, Eq, Ord)
 
