93aec179976a84dc4db8d7d9c780fa009ee7f02a
Adam Walker <adamwalker10@gmail.com>
2020-03-24 21:02:39 +1100

    stream chaining utilities

---
+++ src/Clash/Stream/Chain.hs
@@ -0,0 +1,36 @@
+-- | Utility functions for wiring up streams with backpressure
+module Clash.Stream.Chain (
+    chainStream,
+    VecStream,
+    chainStreamVec
+    ) where
+
+import Clash.Prelude
+
+-- | Convenience function for chaining streams together, correctly connecting up their ready signals
+chainStream 
+    :: (Signal dom b -> Signal dom Bool -> (Signal dom a, Signal dom Bool)) -- ^ Regular stream function accepting and returning backpressure
+    -> (Signal dom Bool -> Signal dom b)                                    -- ^ Stream function earlier in the pipeline
+    -> (Signal dom Bool -> Signal dom a)                                    -- ^ Output stream function
+chainStream downstream upstream readyOut = streamOut
+    where
+    dat                = upstream ready
+    (streamOut, ready) = downstream dat readyOut
+
+type VecStream a b dom m
+    =  Signal dom Bool               -- ^ Downstream ready
+    -> Vec m (Signal dom a)          -- ^ Incoming streams
+    -> (
+            Vec m (Signal dom Bool), -- ^ Upstream readys
+            Signal dom b             -- ^ Outgoing streams
+    )
+
+-- | Convenience function for merging streams together, correctly connecting up their ready signals
+chainStreamVec
+    :: VecStream a b dom m                     -- ^ Regular stream function, accepting and returning backpressure, operating on a vector of streams
+    -> Vec m (Signal dom Bool -> Signal dom a) -- ^ Stream functions earlier in the pipeline
+    -> (Signal dom Bool -> Signal dom b)       -- ^ Output stream function
+chainStreamVec op streams readyOut = out
+    where
+    (readys, out) = op readyOut $ zipWith ($) streams readys
+
