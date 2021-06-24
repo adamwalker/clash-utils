975f24f53991ab2728dd99614ad19a7aa3b24eda
Adam Walker <adamwalker10@gmail.com>
2021-06-24 11:44:18 +1000

    wrappingCounter

--- src/Clash/Counter.hs
+++ src/Clash/Counter.hs
@@ -1,6 +1,7 @@
 module Clash.Counter (
     count,
-    upDownCounter
+    upDownCounter,
+    wrappingCounter
     ) where
 
 import Clash.Prelude
@@ -8,22 +9,37 @@ import Clash.Prelude
 -- | Counts cycles where the input signal is high
 count 
     :: (HiddenClockResetEnable dom, Num a, NFDataX a) 
-    => Signal dom Bool -- ^ Increment signal
+    => a               -- ^ Reset value
+    -> Signal dom Bool -- ^ Increment count
     -> Signal dom a    -- ^ Count output
-count inc = res
+count init inc = res
     where
-    res = regEn 0 inc $ res + 1
+    res = regEn init inc $ res + 1
 
 -- | Counts up and down
 upDownCounter 
     :: (HiddenClockResetEnable dom, Num a, NFDataX a)
-    => Signal dom Bool -- ^ Increment count
+    => a               -- ^ Reset value
+    -> Signal dom Bool -- ^ Increment count
     -> Signal dom Bool -- ^ Decrement count
     -> Signal dom a    -- ^ Count
-upDownCounter up down = count
+upDownCounter init up down = count
     where
     count = register 0 $ func <$> count <*> up <*> down
         where
         func count True  False = count + 1
         func count False True  = count - 1
         func count _     _     = count
+
+-- | Counts up and wraps around at maxBound
+wrappingCounter 
+    :: (HiddenClockResetEnable dom, Num b, Bounded b, Eq b, NFDataX b)
+    => b               -- ^ Reset value
+    -> Signal dom Bool -- ^ Increment count
+    -> Signal dom b    -- ^ Count output
+wrappingCounter init inc = res
+    where
+    res = regEn init inc (wrappingInc <$> res)
+    wrappingInc x
+        | x == maxBound = 0
+        | otherwise     = x + 1
