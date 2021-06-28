22c20415e467da97f2bca365734b1e2008bb6a27
Adam Walker <adamwalker10@gmail.com>
2021-06-28 19:11:02 +1000

    fir filter test consistency

--- tests/FIRFilterSpec.hs
+++ tests/FIRFilterSpec.hs
@@ -44,37 +44,85 @@ macPreAddRealReal c x y a = c * (x + y) + a
 
 liftA4 f w x y z = f <$> w <*> x <*> y <*> z
 
-prop_FilterTransposed :: Vec 16 (Signed 32) -> [Signed 32] -> Bool
-prop_FilterTransposed coeffs input = 
-       take (length input) (simulate @System (goldenFIR coeffs (pure True)) input) 
-    == take (length input) (drop 1 (simulate @System (firTransposed (const (liftA3 macRealReal)) (Clash.reverse coeffs) (pure True)) input))
-
-prop_FilterSystolic :: Vec 16 (Signed 32) -> [Signed 32] -> Bool
-prop_FilterSystolic coeffs input = 
-       take (length input) (simulate @System (goldenFIR coeffs (pure True)) input) 
-    == take (length input) (drop 16 $ simulate @System (firSystolic (const (liftA3 macRealReal)) coeffs (pure True)) input)
-
-prop_FilterSystolicSymmetric :: Vec 16 (Signed 32) -> [Signed 32] -> Bool
-prop_FilterSystolicSymmetric coeffs input = 
-       take (length input) (simulate @System (goldenFIR (coeffs Clash.++ Clash.reverse coeffs) (pure True)) input) 
-    == take (length input) (drop 16 $ simulate @System (firSystolicSymmetric (const (liftA4 macPreAddRealReal)) coeffs (pure True)) input)
-
-prop_FilterSymmetric :: Vec 16 (Signed 32) -> [Signed 32] -> Bool
-prop_FilterSymmetric coeffs input = 
-       take (length input) (simulate @System (Clash.register 0 . goldenFIR (Clash.reverse coeffs Clash.++ coeffs) (pure True)) input) 
-    == take (length input) (simulate @System (firSymmetric (const (liftA4 macPreAddRealReal)) coeffs (pure True)) input)
-
-prop_systolicSymmetric :: Vec 16 (Signed 32) -> Signed 32 -> [Signed 32] -> Bool
-prop_systolicSymmetric coeffs mid input = 
-       take (length input) (simulate @System (goldenFIR (coeffs Clash.++ Clash.singleton mid Clash.++ Clash.reverse coeffs) (pure True)) input)
-    == take (length input) (drop 17 $ simulate @System (firSystolicSymmetricOdd (const (liftA4 macPreAddRealReal)) (coeffs Clash.++ Clash.singleton mid) (pure True)) input)
-
-prop_systolicHalfBand :: Vec 16 (Signed 32) -> Signed 32 -> [Signed 32] -> Bool
-prop_systolicHalfBand coeffs mid input = 
-       take (length input) (simulate @System (goldenFIR (coeffs' Clash.++ Clash.singleton mid Clash.++ Clash.reverse coeffs') (pure True)) input)
-    == take (length input) (drop 17 $ simulate @System (firSystolicHalfBand (const (liftA4 macPreAddRealReal)) (coeffs Clash.++ Clash.singleton mid) (pure True)) input)
+prop_FilterTransposed :: Vec 16 (Signed 32) -> [Signed 32] -> Property
+prop_FilterTransposed coeffs input = expect === result
     where
-    coeffs' = Clash.init (Clash.merge coeffs (Clash.repeat 0))
+    expect 
+        = take (length input) 
+        $ sample @System 
+        $ goldenFIR coeffs (pure True) (fromList $ 0 : input ++ repeat 0) 
+    result 
+        = take (length input) 
+        $ drop 1 
+        $ sample @System 
+        $ firTransposed (const (liftA3 macRealReal)) (Clash.reverse coeffs) (pure True) (fromList $ 0 : input ++ repeat 0)
+
+prop_FilterSystolic :: Vec 16 (Signed 32) -> [Signed 32] -> Property
+prop_FilterSystolic coeffs input = expect === result
+    where
+    expect 
+        = take (length input) 
+        $ sample @System 
+        $ goldenFIR coeffs (pure True) (fromList $ 0 : input ++ repeat 0)
+    result 
+        = take (length input) 
+        $ drop 16 
+        $ sample @System 
+        $ firSystolic (const (liftA3 macRealReal)) coeffs (pure True) (fromList $ 0 : input ++ repeat 0)
+
+prop_FilterSystolicSymmetric :: Vec 16 (Signed 32) -> [Signed 32] -> Property
+prop_FilterSystolicSymmetric coeffs input = expect === result
+    where
+    expect 
+        = take (length input) 
+        $ sample @System 
+        $ goldenFIR (coeffs Clash.++ Clash.reverse coeffs) (pure True) (fromList $ 0 : input ++ repeat 0)
+    result
+        = take (length input) 
+        $ drop 16 
+        $ sample @System 
+        $ firSystolicSymmetric (const (liftA4 macPreAddRealReal)) coeffs (pure True) (fromList $ 0 : input ++ repeat 0)
+
+prop_FilterSymmetric :: Vec 16 (Signed 32) -> [Signed 32] -> Property
+prop_FilterSymmetric coeffs input = expect === result
+    where
+    expect
+        = take (length input) 
+        $ sample @System 
+        $ goldenFIR (Clash.reverse coeffs Clash.++ coeffs) (pure True) (fromList $ 0 : input ++ repeat 0)
+    result
+        = take (length input) 
+        $ drop 1
+        $ sample @System 
+        $ firSymmetric (const (liftA4 macPreAddRealReal)) coeffs (pure True) (fromList $ 0 : input ++ repeat 0)
+
+prop_systolicSymmetric :: Vec 16 (Signed 32) -> Signed 32 -> [Signed 32] -> Property
+prop_systolicSymmetric coeffs mid input = expect === result
+    where
+    expect 
+        = take (length input) 
+        $ sample @System 
+        $ goldenFIR (coeffs Clash.++ Clash.singleton mid Clash.++ Clash.reverse coeffs) (pure True) (fromList $ 0 : input ++ repeat 0)
+    result
+        = take (length input) 
+        $ drop 17 
+        $ sample @System 
+        $ firSystolicSymmetricOdd (const (liftA4 macPreAddRealReal)) (coeffs Clash.++ Clash.singleton mid) (pure True) (fromList $ 0 : input ++ repeat 0)
+
+prop_systolicHalfBand :: Vec 16 (Signed 32) -> Signed 32 -> [Signed 32] -> Property
+prop_systolicHalfBand coeffs mid input = expect === result
+    where
+    expect 
+        = take (length input) 
+        $ sample @System 
+        $ goldenFIR (coeffs' Clash.++ Clash.singleton mid Clash.++ Clash.reverse coeffs') (pure True) (fromList $ 0 : input ++ repeat 0)
+    result 
+        = take (length input) 
+        $ drop 17 
+        $ sample @System 
+        $ firSystolicHalfBand (const (liftA4 macPreAddRealReal)) (coeffs Clash.++ Clash.singleton mid) (pure True) (fromList $ 0 : input ++ repeat 0)
+    coeffs' 
+        = Clash.init (Clash.merge coeffs (Clash.repeat 0))
 
 streamList 
     :: (HiddenClockResetEnable dom, NFDataX a)
