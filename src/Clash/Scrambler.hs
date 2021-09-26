3ad8b35a03afbdf98ddfca1e33f7989c1797c3ff
Adam Walker <adamwalker10@gmail.com>
2021-09-26 13:19:39 +1000

    split out scrambler steps

--- src/Clash/Scrambler.hs
+++ src/Clash/Scrambler.hs
@@ -1,10 +1,26 @@
 {-| Multiplicative scrambler and descrambler: https://en.wikipedia.org/wiki/Scrambler. -}
 module Clash.Scrambler (
+    scramblerStep,
     scrambler,
+    descramblerStep,
     descrambler
     ) where
 
 import Clash.Prelude
+
+scramblerStep 
+    :: KnownNat n
+    => BitVector n
+    -> Vec (n + 1) Bool 
+    -> Bool 
+    -> (Vec (n + 1) Bool, Bool)
+scramblerStep poly state input = (output +>> state, output)
+    where
+    output :: Bool
+    output = foldl1 xor 
+        $  zipWith (.&.) (init state) (unpack poly) 
+        :< last state 
+        :< input
 
 scrambler 
     :: forall dom n. (HiddenClockResetEnable dom, KnownNat n)
@@ -12,16 +28,21 @@ scrambler
     -> BitVector n       -- ^ Polynomial
     -> Signal dom Bool   -- ^ Input bit
     -> Signal dom Bool   -- ^ Output bit
-scrambler initial poly input = mealy scramblerStep (unpack initial) input
+scrambler initial poly input = mealy (scramblerStep poly) (unpack initial) input
+
+descramblerStep 
+    :: KnownNat n
+    => BitVector n
+    -> Vec (n + 1) Bool 
+    -> Bool 
+    -> (Vec (n + 1) Bool, Bool)
+descramblerStep poly state input = (input +>> state, output)
     where 
-    scramblerStep :: Vec (n + 1) Bool -> Bool -> (Vec (n + 1) Bool, Bool)
-    scramblerStep state input = (output +>> state, output)
-        where
-        output :: Bool
-        output = foldl1 xor 
-            $  zipWith (.&.) (init state) (unpack poly) 
-            :< last state 
-            :< input
+    output :: Bool
+    output = foldl1 xor 
+        $  zipWith (.&.) (init state) (unpack poly) 
+        :< (last state) 
+        :< input
 
 descrambler 
     :: forall dom n. (HiddenClockResetEnable dom, KnownNat n)
@@ -29,14 +50,5 @@ descrambler
     -> BitVector n       -- ^ Polynomial
     -> Signal dom Bool   -- ^ Input bit
     -> Signal dom Bool   -- ^ Output bit
-descrambler initial poly input = mealy descramblerStep (unpack initial) input
-    where
-    descramblerStep :: Vec (n + 1) Bool -> Bool -> (Vec (n + 1) Bool, Bool)
-    descramblerStep state input = (input +>> state, output)
-        where 
-        output :: Bool
-        output = foldl1 xor 
-            $  zipWith (.&.) (init state) (unpack poly) 
-            :< (last state) 
-            :< input
+descrambler initial poly input = mealy (descramblerStep poly) (unpack initial) input
 
