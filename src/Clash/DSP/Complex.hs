7232597fc703bb3d889d0c6478dbe24342c383ad
Adam Walker <adamwalker10@gmail.com>
2021-10-02 17:00:29 +1000

    complex pipeline multiplication

--- src/Clash/DSP/Complex.hs
+++ src/Clash/DSP/Complex.hs
@@ -66,3 +66,59 @@ cMul3 (xr :+ xi) (yr :+ yi)
     k2 = xr `mul` (yi `sub` yr)
     k3 = xi `mul` (yr `add` yi)
 
+cMulPipe
+    :: HiddenClockResetEnable dom
+    => ExtendingNum a b 
+    => ExtendingNum (MResult a b) (MResult a b)
+    => NFDataX (MResult a b)
+    => NFDataX (AResult (MResult a b) (MResult a b))
+    => Signal dom (Complex a)
+    -> Signal dom (Complex b)
+    -> Signal dom (Complex (AResult (MResult a b) (MResult a b)))
+cMulPipe x y 
+    = liftA2 (:+) rr ri
+    where
+    --Products
+    xryr = register (errorX "initial xryr") $ liftA2 mul (realPart <$> x) (realPart <$> y)
+    xiyi = register (errorX "initial xiyi") $ liftA2 mul (imagPart <$> x) (imagPart <$> y)
+    xryi = register (errorX "initial xryi") $ liftA2 mul (realPart <$> x) (imagPart <$> y)
+    xiyr = register (errorX "initial xiyr") $ liftA2 mul (imagPart <$> x) (realPart <$> y)
+    --Sums
+    rr   = register (errorX "initial rr")   $ liftA2 sub xryr xiyi
+    ri   = register (errorX "initial ri")   $ liftA2 add xryi xiyr
+
+cMul3Pipe
+    :: HiddenClockResetEnable dom
+    => ExtendingNum b b
+    => ExtendingNum a a
+    => ExtendingNum a (AResult b b)
+    => ExtendingNum b (AResult a a)
+    => ExtendingNum (MResult b (AResult a a)) (MResult a (AResult b b))
+    => NFDataX (AResult a a) 
+    => NFDataX (AResult b b) 
+    => NFDataX (MResult b (AResult a a))
+    => NFDataX (MResult a (AResult b b))
+    => NFDataX (AResult (MResult b (AResult a a)) (MResult a (AResult b b)))
+    => NFDataX a
+    => NFDataX b
+    => Signal dom (Complex a)
+    -> Signal dom (Complex b)
+    -> Signal dom (Complex (AResult (MResult b (AResult a a)) (MResult a (AResult b b))))
+cMul3Pipe x y
+    =  liftA2 (:+) rr ri
+    where
+    --Sums
+    xSum    = register (errorX "initial xSum")    $ liftA2 add (realPart <$> x) (imagPart <$> x)
+    yDiff   = register (errorX "initial yDiff")   $ liftA2 sub (imagPart <$> y) (realPart <$> y)
+    ySum    = register (errorX "initial ySum")    $ liftA2 add (realPart <$> y) (imagPart <$> y)
+    --Delays
+    xD      = register (errorX "initial xD") x
+    yD      = register (errorX "initial yD") y
+    --Products
+    yrXSum  = register (errorX "initial yrXSum")  $ liftA2 mul (realPart <$> yD) xSum
+    xrYDiff = register (errorX "initial xrYDiff") $ liftA2 mul (realPart <$> xD) yDiff
+    xiYSum  = register (errorX "initial xiYSum")  $ liftA2 mul (imagPart <$> xD) ySum
+    --Sums
+    rr      = register (errorX "initial rr")      $ liftA2 sub yrXSum xiYSum
+    ri      = register (errorX "initial ri")      $ liftA2 add yrXSum xrYDiff
+
--- tests/ComplexSpec.hs
+++ tests/ComplexSpec.hs
@@ -11,6 +11,8 @@ import Clash.DSP.Complex
 
 spec = describe "Complex numbers" $ do
     specify "Three multipler complex multiplication gives the correct result" $ property prop_complex3Mul
+    specify "Pipelined multiplier gives the correct result"                   $ property prop_complexPipe
+    specify "Pipelined three multiplier multiplier gives the correct result"  $ property prop_complexPipe3Mul
 
 prop_complex3Mul :: Complex (Signed 16) -> Complex (Signed 16) -> Property
 prop_complex3Mul x y = expect === (Clash.truncateB <$> result)
@@ -19,3 +21,20 @@ prop_complex3Mul x y = expect === (Clash.truncateB <$> result)
     expect =  cMul x y 
     result :: Complex (Signed 34)
     result =  cMul3 x y
+
+prop_complexPipe :: [(Complex (Signed 16), Complex (Signed 16))] -> Property
+prop_complexPipe cs = noShrinking $ expect === take (length cs) (drop 3 result)
+    where
+    expect :: [Complex (Signed 33)]
+    expect =  map (uncurry cMul) cs
+    result :: [Complex (Signed 33)]
+    result =  sample @System $ cMulPipe (fromList $ 0 : map fst cs ++ repeat 0) (fromList $ 0 : map snd cs ++ repeat 0)
+
+prop_complexPipe3Mul :: [(Complex (Signed 16), Complex (Signed 16))] -> Property
+prop_complexPipe3Mul cs = noShrinking $ expect === map (fmap Clash.truncateB) (take (length cs) (drop 4 result))
+    where
+    expect :: [Complex (Signed 33)]
+    expect =  map (uncurry cMul) cs
+    result :: [Complex (Signed 34)]
+    result =  sample @System $ cMul3Pipe (fromList $ 0 : map fst cs ++ repeat 0) (fromList $ 0 : map snd cs ++ repeat 0)
+
